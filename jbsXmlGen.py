55a7e6cc3024e4600a03c94cb3d72ed2986b0821
Chirag Vartak <vartak.chirag.8@gmail.com>
2016-09-03 18:49:35 +0530

    First commit

---
+++ jbsXmlGen.py
@@ -0,0 +1,82 @@
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.dom import minidom
+import math
+
+output_file = 'jbs.xml'
+
+# Set user and bs from file here.
+user = [1, 3, 6, 7, 9, 12, 15]
+bs = [2, 5, 8, 11, 14]
+K = 3 # Number of colors
+
+infinity = math.inf
+
+# For pretty xml output
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="    ")
+
+
+instance = Element('instance')
+
+presentation = SubElement(instance, 'presentation')
+presentation.set('name', 'JBSProblem')
+presentation.set('maxConstraintArity', 'dunno')
+presentation.set('maximize', 'false')
+presentation.set('format', 'XCSP 2.1_FRODO')
+
+agents = SubElement(instance, 'agents')
+agents.set('nbAgents', str(len(user)))
+
+for x in user:
+    agent = SubElement(agents, 'agent')
+    agent.set('name', 'agent'+str(x))
+
+
+# Create all the domains here
+
+domains = SubElement(instance, 'domains')
+domains.set('nbDomains', str(len(user)))
+
+for u in user:
+    # Left basestation
+    left_bs = None
+    min_dist = infinity
+    for b in bs:
+        if b < u and abs(u - b) < min_dist:
+            left_bs = b
+            min_dist = abs(u - b)
+
+    # Right Basestation
+    right_bs = None
+    min_dist = infinity
+    for b in bs:
+        if b > u and abs(u - b) < min_dist:
+            right_bs = b
+            min_dist = abs(u - b)
+
+    # Both left and right BS cannot be 'None'.
+    assert left_bs != None or right_bs != None
+
+    domain = SubElement(domains, 'domain')
+    domain.set('name', 'domain_of_agent' + str(u))
+
+    B = []
+    if left_bs != None:
+        B.append(left_bs)
+    if right_bs != None:
+        B.append(right_bs)
+    D = [(u*1000000 + b*1000 + k) for b in B for k in range(1, K+1)]
+
+    domain.set('nbValues', str(len(D)))
+    domain.text = ' '.join(str(x) for x in D)
+
+
+
+# Writing to the output_file
+opfile = open(output_file, 'w')
+print(prettify(instance), file=opfile)
+opfile.close()
---
+++ xmlpractice.py
@@ -0,0 +1,64 @@
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.dom import minidom
+
+output_file = 'example.xml'
+
+# For pretty xml output
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="    ")
+
+data = Element('data')
+
+country = SubElement(data, 'country')
+country.set('name', 'Liechtenstein')
+
+rank = SubElement(country, 'rank')
+rank.text = "1"
+year = SubElement(country, 'year')
+year.text = "2008"
+gdppc = SubElement(country, 'gdppc')
+gdppc.text = "141100"
+neighbor = SubElement(country, 'neighbor')
+neighbor.set('name', 'Austria')
+neighbor.set('direction', 'E')
+neighbor = SubElement(country, 'neighbor')
+neighbor.set('name', 'Switzerland')
+neighbor.set('direction', 'W')
+
+country = SubElement(data, 'country')
+country.set('name', 'Singapore')
+
+rank = SubElement(country, 'rank')
+rank.text = '4'
+year = SubElement(country, 'year')
+year.text = "2011"
+gdppc = SubElement(country, 'gdppc')
+gdppc.text = "59900"
+neighbor = SubElement(country, 'neighbor')
+neighbor.set('name', 'Malaysia')
+neighbor.set('direction', 'N')
+
+country = SubElement(data, 'country')
+country.set('name', 'Panama')
+
+rank = SubElement(country, 'rank')
+rank.text = "68"
+year = SubElement(country, 'year')
+year.text = "2011"
+gdppc = SubElement(country, 'gdppc')
+gdppc.text = "13600"
+neighbor = SubElement(country, 'neighbor')
+neighbor.set('name', 'Costa Rica')
+neighbor.set('direction', 'W')
+neighbor = SubElement(country, 'neighbor')
+neighbor.set('name', 'Colombia')
+neighbor.set('direction', 'E')
+
+# Writing to the output_file
+opfile = open(output_file, 'w')
+print(prettify(data), file=opfile)
+opfile.close()
